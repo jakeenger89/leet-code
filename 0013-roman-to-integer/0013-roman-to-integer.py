b5821002c30eb7c3796990585a81e5d6863e3b2c
jakeenger89 <140981112+jakeenger89@users.noreply.github.com>
2023-12-25 11:49:25 -0800

    Time: 24 ms (80.03%), Space: 13.3 MB (45.04%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def romanToInt(self, s):
+        roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+        
+        prev_value = 0
+        for char in reversed(s):
+            value = roman_dict[char]
+
+            if value < prev_value:
+                result -= value
+            else:
+                result += value
+
+            prev_value = value
+
+        return result
