e50c87837e250dcce18c2dddfed7ff42210135fe
jakeenger89 <140981112+jakeenger89@users.noreply.github.com>
2024-01-17 12:44:22 -0800

    Time: 16 ms (84.69%), Space: 13.6 MB (15.79%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -2,9 +2,11 @@ from collections import Counter
 
 class Solution(object):
     def uniqueOccurrences(self, arr):
+        #create a counter for how many times the number shows up
         counted = Counter(arr)
         occurance = set()
-        
+        #now for loop over the counted num values, if any are duplicates return false otherwise add them to
+        #occurance to check for duplcates
         for count in counted.values():
             if count in occurance:
                 return False
