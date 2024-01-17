a98916541fa102401748a52e4bc5f56d4a05a3f8
jakeenger89 <140981112+jakeenger89@users.noreply.github.com>
2024-01-17 13:15:14 -0800

    Time: 17 ms (49.96%), Space: 13.2 MB (83.16%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -2,8 +2,11 @@ class Solution:
     # @param n, an integer
     # @return an integer
     def reverseBits(self, n):
+        #formate to a string
         binary_string = format(n, '032b')
+        #reverse teh string
         reverseN = binary_string[::-1]
+        #convert string into its unsigned Int
         unsignedInt = int(reverseN, 2)
         
         return unsignedInt
