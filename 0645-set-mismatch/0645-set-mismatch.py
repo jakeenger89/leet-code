1b5d958f574d50f75573756e4c5b70eab94efb93
jakeenger89 <140981112+jakeenger89@users.noreply.github.com>
2024-01-24 10:08:49 -0800

    Time: 136 ms (86.94%), Space: 12.9 MB (90.21%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -14,3 +14,5 @@ class Solution:
                 missing = i + 1
 
         return [duplicate, missing]
+
+# if your gonna have some dumbass rule about [2,2] and the answer being [2,1] make tha tclear in teh discription you only display in order acending so make it clear.
