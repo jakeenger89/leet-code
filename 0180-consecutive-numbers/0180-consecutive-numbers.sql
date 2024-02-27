a42827bd14922c4c29dfe3f7f3eb6639896406c4
jakeenger89 <140981112+jakeenger89@users.noreply.github.com>
2024-02-27 13:24:22 -0800

    Time: 1270 ms (16.81%), Space: 0B (100.00%) - LeetHub

---
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT num as ConsecutiveNums
+FROM (
+    SELECT num,
+           LEAD(num, 1) OVER (ORDER BY id) AS next_num,
+           LEAD(num, 2) OVER (ORDER BY id) AS next_next_num
+    FROM Logs
+) AS subquery
+WHERE num = next_num AND num = next_next_num;
