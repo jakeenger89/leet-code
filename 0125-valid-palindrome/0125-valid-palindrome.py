c1e97576d7d2a74dbeafe80cf12526b77be17340
jakeenger89 <140981112+jakeenger89@users.noreply.github.com>
2024-03-06 11:41:16 -0800

    Time: 29 ms (65.78%), Space: 13.5 MB (55.16%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,11 +1,13 @@
 class Solution(object):
     def isPalindrome(self, s):
-        #we want to make a copy of the word w/o special chars
-        word = ""
-        for letter in s:
-            #check if letters are letters before adding to word
-            if letter.isalnum():
-                #set to lower so letters are consistantly the same
-                word += letter.lower()
-                #if they are the same this will return true
-        return word == "".join(reversed(word))
+        #make lowercase 
+        s = s.lower()
+        #remove spaces 
+        s = s.replace(" ", "")
+        #remove all non chars
+        s = ''.join(char for char in s if char.isalnum())
+        
+        if s == s[::-1]:
+            return True
+        else:
+            False
