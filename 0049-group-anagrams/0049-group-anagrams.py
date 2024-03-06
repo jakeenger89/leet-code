5cf741256e9fb1e3a6d43c0b01671ec990c6b20c
jakeenger89 <140981112+jakeenger89@users.noreply.github.com>
2024-03-06 11:30:42 -0800

    Time: 62 ms (92.69%), Space: 15.6 MB (76.91%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,14 +1,10 @@
-from collections import defaultdict
-
 class Solution(object):
     def groupAnagrams(self, strs):
-        if not strs:
-            return ""
-        result = {}
-        for i in strs:
-            x="".join(sorted(i))
-            if x in result:
-                result[x].append(i)
+        anagram_dict = {}
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            if sorted_word in anagram_dict:
+                anagram_dict[sorted_word].append(word)
             else:
-                result[x] = [i]
-        return list(result.values())
+                anagram_dict[sorted_word] = [word]
+        return list(anagram_dict.values())
