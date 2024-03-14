83b175bb5f74b9f43c6319c5b3be61a07df4cf39
jakeenger89 <140981112+jakeenger89@users.noreply.github.com>
2024-03-14 12:45:55 -0700

    Time: 34 ms (19.92%), Space: 12.3 MB (63.42%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        for index, value in enumerate(nums):
+            if value == target:
+                return index
+            elif value > target:
+                return index
+        return len(nums)
